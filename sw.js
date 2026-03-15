6793e487ad265479f15c615d3098f823b9365c32
mariapilar131268-jpg <mariapilar131268@gmail.com>
2026-03-15 13:58:48 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='transpi2026-v19';
+const CACHE='transpi2026-v20';
 const ASSETS=['./','./?source=pwa','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
