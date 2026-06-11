8639f8473003fe3d68f19f46ab7f77646260bb90
mariapilar131268-jpg <mariapilar131268@gmail.com>
2026-06-11 06:06:27 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'transpi2026-v39';
+const CACHE = 'transpi2026-v40';
 const ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
@@ -6,25 +6,13 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-    )).then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      if(res.ok && e.request.url.startsWith('https://')) {
-        const rc = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, rc));
-      }
-      return res;
-    }))
-  );
-});
-
-self.addEventListener('message', e => {
-  if(e.data === 'SKIP_WAITING') self.skipWaiting();
+  e.respondWith(fetch(e.request).then(r => {
+    const clone = r.clone();
+    caches.open(CACHE).then(c => c.put(e.request, clone));
+    return r;
+  }).catch(() => caches.match(e.request)));
 });
